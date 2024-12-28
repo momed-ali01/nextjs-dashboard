7d4acec2bf7d31fcdff1af7d5eaa00c171ea090d
momed-ali01 <demahomali01@gmail.com>
2024-12-28 09:11:46 +0300

    Add Prettier configuration and update code formatting across the project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+    return <p className="p-4 pt-2">Customers Page</p>
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,6 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
-const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
+const inter = Inter({ subsets: ["latin"] })
+const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] })
 
-export { inter, lusitana };
+export { inter, lusitana }
