d14c73ed24c6469662ab05e7ee69c9e8f638121d
momed-ali01 <demahomali01@gmail.com>
2024-12-28 15:42:48 +0300

    Enhance error handling in invoice creation and update functions; clean up unused imports in various components

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,6 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         // If a database error occurs, return a more specific error.
         return {
             message: "Database Error: Failed to Create Invoice.",
+            error,
         };
     }
 
@@ -102,7 +103,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
     } catch (error) {
-        return { message: "Database Error: Failed to Update Invoice." };
+        return { message: "Database Error: Failed to Update Invoice.", error };
     }
 
     revalidatePath("/dashboard/invoices");
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo"
-import {
-    ArrowDownCircleIcon,
-    ArrowLongUpIcon,
-    ArrowRightIcon,
-    LinkIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
